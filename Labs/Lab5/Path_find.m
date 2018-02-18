b701e774f154c03405ffa4318941cde7417bc585
Kamal Carter <kamalrcarter@gmail.com>
2018-02-18 00:37:16 -0500

    All The Object Coordinates

--- Labs/Lab5/Path_find.m
+++ Labs/Lab5/Path_find.m
@@ -8,15 +8,15 @@ width = 84; %inches
 height = 48;
 mat = zeros(height * res, width * res);
 
-%Obstacles Coordinates from left to right objects and coords CCW:
-P1x = [1,22.75];
-P1y = [31.5,30.25];
-P2x = [5.25,26.25];
-P2y = [36,34.5];
-P3x = [22.5,30.5];
-P3y = [19.5,30.5];
-P4x = [18.25,26.25];
-P4y = [15.25,26.25];
+%Obstacles Coordinates from left to right objects and coords CCW from top
+P1x = [1   ,  22.75 ,  47.5 ,  55.25, 68.00, 72.00];
+P1y = [31.5,  30.25 ,  35.25,  18.75, 26.00, 42.50];
+P2x = [5.25,  26.25 ,  51.5 ,  56.50, 72.75, 76.25];
+P2y = [36  ,  34.5  ,  30.75,  14.75, 23.50, 38.25];
+P3x = [22.5,  30.5  ,  34.00,  52.50, 69.00, 72.00];
+P3y = [19.5,  30.5  ,  14.75,  10.50, 17.75, 34.00];
+P4x = [18.25, 26.25 ,  29.75,  48.00, 64.25, 84.00];
+P4y = [15.25, 26.25 ,  19.25,  14.25, 21.50, 38.25];
 
 
 
@@ -34,11 +34,11 @@ for row = 1:rows
     objectList(row).X1 =  P1x(row);
     objectList(row).Y1 =  P1y(row);
     objectList(row).X2 =  P2x(row);
-    objectList(row).Y2 = P2y(row);
-    objectList(row).X3 = P3x(row);
-    objectList(row).Y3 = P3y(row);
-    objectList(row).X4 = P4x(row);
-    objectList(row).Y4 = P4y(row);
+    objectList(row).Y2 =  P2y(row);
+    objectList(row).X3 =  P3x(row);
+    objectList(row).Y3 =  P3y(row);
+    objectList(row).X4 =  P4x(row);
+    objectList(row).Y4 =  P4y(row);
     
 end
 
